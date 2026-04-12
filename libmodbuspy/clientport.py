6e50d7cb7ace5bd06fa7ac7fe1589fabaff192d7
serhmarch <mrchsrg@gmail.com>
2026-04-12 11:31:51 +0000

    Add FC08.diagnosticsReturnBusCommunicationErrorCount for client

--- libmodbuspy/clientport.py
+++ libmodbuspy/clientport.py
@@ -1003,6 +1003,48 @@ class ModbusClientPort(ModbusObject, ModbusInterface):
         else:
             return None
 
+    def _diagnosticsReturnBusCommunicationErrorCount(self, client:ModbusObject, unit: int) -> bytes:
+        """Diagnostics subfunction returns the quantity of CRC errors encountered by the remote device
+        since its last restart, clear counters operation, or power-up.
+        
+        Args:
+            client: The client object making the request.
+            unit: Address of the remote Modbus device.
+            
+        Returns:
+            * `bytes` array of size 2 containing the register value.
+            * `None` when operation is not finished yet (only for nonblocking mode).
+
+        Raises:
+            Exceptions with base class `libmodbuspy.ModbusException` on error.
+        """
+        status = self.getRequestStatus(client)        
+        if status == ModbusClientPort.RequestStatus.Enable:
+            # Prepare request buffer
+            subfunc = MBF_DIAGNOSTICS_RETURN_BUS_COMMUNICATION_ERROR_COUNT
+            self._buff = bytearray(4)
+            self._buff[0] = (subfunc >> 8) & 0xFF # Sub function - MS BYTE
+            self._buff[1] = subfunc & 0xFF        # Sub function - LS BYTE
+            self._buff[2] = 0x00                  # Must be 0x00
+            self._buff[3] = 0x00                  # Must be 0x00
+            self._subfunc = subfunc
+            status = ModbusClientPort.RequestStatus.Process        
+        if status == ModbusClientPort.RequestStatus.Process:
+            buff = self._request(unit, MBF_DIAGNOSTICS, self._buff)
+            if buff is None:
+                return None
+            if self._isBroadcast():
+                return bytes()
+            if len(buff) != 4:
+                self._raiseError(StatusCode.Status_BadNotCorrectResponse, "Incorrect received data size")
+            outSubfunc = buff[1] | (buff[0] << 8)
+            if outSubfunc != self._subfunc:
+                self._raiseError(StatusCode.Status_BadNotCorrectResponse, "Diagnostics sub-function is not match received one")
+            self._setStatusCompleted(StatusCode.Status_Good)
+            return bytes([buff[3], buff[2]]) # Register value - LS BYTE first
+        else:
+            return None
+
     def _getCommEventCounter(self, client:ModbusObject, unit: int) -> bytes:
         """ Get communication event counter from Modbus device.
 
